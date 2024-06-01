d3be5e087123839d23cbd8176b521412086d712f
ru0ya <viktaangi02@gmail.com>
2024-06-01 07:00:54 +0300

    clean up

---
+++ stage_one/user_data.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from datetime import datetime
+
+
+current_day = datetime.now().strftime("%A")
+utc_time = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
+data = {
+        "slack_name": "Victor Ruoya",
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": "backend",
+        "github_file_url": "https://github.com/ru0ya/Hng_Backend/blob/main/stage_one/main.py",
+        "github_repo_url": "https://github.com/ru0ya/Hng_Backend/tree/main/stage_one",
+        "status_code": 200
+        }
